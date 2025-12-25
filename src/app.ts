952e8c5d64830d090862375c6aa4c6a2690831f5
Antonin Gontier <gontier.antonin@orange.fr>
2025-12-25 22:04:41 +0100

    ✨display sentence

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,68 @@
-import express from 'express';
+import express, { urlencoded } from 'express';
+import { engine } from 'express-handlebars';
+import path from 'path';
 import { getRandomSentence } from './services/tatoebaConnector';
 import { analyzeSentence } from './services/satzklarConnector';
+import { text } from 'stream/consumers';
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+
 app.get('/', async (req, res) => {
-  const sentence = await getRandomSentence();
-  const analyzis = await analyzeSentence(sentence.text);
+  //const sentence = await getRandomSentence();
+  const sentence = {
+    text: 'Ich habe einen Hund',
+    translations:
+      [
+        {
+          lang: 'eng',
+          text: 'I have a dog',
+        }
+      ],
+    audios: [{
+      download_url: ""
+    }]
+  };
 
-  const analyzisCore = analyzis.data.output.component.analysis;
-  const analyzisTree = analyzis.data.output.component.tree;
-
-  const html = `
-    <p>${sentence.text}</p>
-    <p>${sentence.translations.filter(t => t.lang === 'eng')[0].text}</p>
-    <audio controls src="${sentence.audios[0].download_url}"></audio>
-    <p>${analyzisCore.structure}</p>
-    <p>${analyzisCore.wordOrder}</p>
-    <p>${analyzisCore.caseUsage}</p>
-    <p>${analyzisCore.pragmatics.explanation}</p>
-    ${
-      analyzisTree.children.map(c => c.word + ' ' + c.description)
+  // const html = `
+  //   <p>${sentence.text}</p>
+  //   <p>${sentence.translations.filter(t => t.lang === 'eng')[0].text}</p>
+  //   <audio controls src="${sentence.audios[0].download_url}"></audio>
+  //   <p>${analyzisCore.structure}</p>
+  //   <p>${analyzisCore.wordOrder}</p>
+  //   <p>${analyzisCore.caseUsage}</p>
+  //   <p>${analyzisCore.pragmatics.explanation}</p>
+  //   ${
+  //     analyzisTree.children.map(c => c.word + ' ' + c.description)
+  //   }
+  // `;
+  res.render('home', { 
+    sentence: {
+      german: sentence.text,
+      english: sentence.translations.filter(t => t.lang === 'eng')[0].text,
+      audio: sentence.audios[0].download_url,
+      urlEncoded: encodeURI(sentence.text)
     }
-  `
-  res.send(html);
+  });
+});
+
+app.get('/analyzis/:sentence', async (req, res) => {
+  // const analyzis = await analyzeSentence(req.params.sentence);
+
+  // const analyzisCore = analyzis.data.output.component.analysis;
+  // const analyzisTree = analyzis.data.output.component.tree;
+
+  res.render('analyzis', {
+    layout: false,
+    analyzis: {
+
+    }
+  });
 });
 
 app.listen(port, () => {
--- src/services/tatoebaConnector.ts
+++ src/services/tatoebaConnector.ts
@@ -3,7 +3,7 @@ import { Sentence, SentenceSearchResult } from "../types/sentence";
  
 const getRandomSentence = async (): Promise<Sentence> => {
   const sentences = await ky(
-    "https://api.tatoeba.org/unstable/sentences?lang=deu&word_count=4-20&has_audio=yes&trans%3Alang=eng&trans%3Acount=%210&sort=random&showtrans%3Alang=eng",
+    "https://api.tatoeba.org/unstable/sentences?lang=deu&word_count=6-20&has_audio=yes&trans%3Alang=eng&trans%3Acount=%210&sort=random&showtrans%3Alang=eng",
     {
       method: "GET"
     }
