9567ef2faa7a6b16d630ef5e910baa0605ab9362
Antonin Gontier <gontier.antonin@orange.fr>
2025-12-26 01:40:56 +0100

    ✨full front

--- src/app.ts
+++ src/app.ts
@@ -14,33 +14,8 @@ app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', async (req, res) => {
-  //const sentence = await getRandomSentence();
-  const sentence = {
-    text: 'Ich habe einen Hund',
-    translations:
-      [
-        {
-          lang: 'eng',
-          text: 'I have a dog',
-        }
-      ],
-    audios: [{
-      download_url: ""
-    }]
-  };
+  const sentence = await getRandomSentence();
 
-  // const html = `
-  //   <p>${sentence.text}</p>
-  //   <p>${sentence.translations.filter(t => t.lang === 'eng')[0].text}</p>
-  //   <audio controls src="${sentence.audios[0].download_url}"></audio>
-  //   <p>${analyzisCore.structure}</p>
-  //   <p>${analyzisCore.wordOrder}</p>
-  //   <p>${analyzisCore.caseUsage}</p>
-  //   <p>${analyzisCore.pragmatics.explanation}</p>
-  //   ${
-  //     analyzisTree.children.map(c => c.word + ' ' + c.description)
-  //   }
-  // `;
   res.render('home', { 
     sentence: {
       german: sentence.text,
@@ -52,18 +27,58 @@ app.get('/', async (req, res) => {
 });
 
 app.get('/analyzis/:sentence', async (req, res) => {
-  // const analyzis = await analyzeSentence(req.params.sentence);
+  const analyzis = await analyzeSentence(req.params.sentence);
 
-  // const analyzisCore = analyzis.data.output.component.analysis;
-  // const analyzisTree = analyzis.data.output.component.tree;
+  const analyzisCore = analyzis.data.output.component.analysis;
+  const analyzisTree = analyzis.data.output.component.tree;
+
+  console.log(analyzisTree.children)
 
   res.render('analyzis', {
     layout: false,
     analyzis: {
-
+      structure: analyzisCore.structure,
+      wordOrder: analyzisCore.wordOrder,
+      caseUsage: analyzisCore.caseUsage,
+      explanation: analyzisCore.pragmatics.explanation,
+      tree: analyzisTree.children
+        .filter(c => c.component !== 'Interpunktion')
+        .map(c => {
+          if (c.word === c.children?.[0]?.word) { // flatten
+            return {
+              word: c.word,
+              detail: [
+                {
+                  type: c.component,
+                  description: c.description
+                },
+                {
+                  type: c.children[0].component,
+                  description: c.children[0].description
+                }
+              ]
+            }
+          }
+          return { // remap
+            ...treeComponentMapper(c),
+            children: c.children?.map(treeComponentMapper)
+          }
+        })
     }
   });
 });
+
+function treeComponentMapper(component) {
+  return {
+    word: component.word,
+    detail: [
+      {
+        type: component.component,
+        description: component.description
+      }
+    ]
+  };
+}
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
